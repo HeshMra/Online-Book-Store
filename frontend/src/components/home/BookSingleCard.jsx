98884a9a7fd1a548a0b8673783a867edf4b4c1ff
heshith <heshith619@gmail.com>
2023-09-20 12:53:08 +0530

    implement BookModal to show single book details

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -5,7 +5,8 @@ import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineDelete } from 'react-icons/md';
 import { useState } from 'react';
-// import BookModal from './BookModal';
+import BookModal from './BookModal';
+
 
 const BookSingleCard = ({ book }) => {
   const [showModal, setShowModal] = useState(false);
